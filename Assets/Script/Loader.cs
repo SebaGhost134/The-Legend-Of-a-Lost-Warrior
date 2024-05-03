23ca4e6d904956c0d42d23c8da62e78758751821
SuperBellaca <antonanami02@gmail.com>
2024-05-03 16:01:24 -0400

    Script
    Script para cambiar de escena

---
+++ Assets/Script/Loader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager;
+
+public static class Loader 
+{
+    public enum Scene{
+        
+    }
+     public static Load(){
+        SceneManager.LoadScene();
+     }
+}
