9bdf7f128d4e7929630717cbabd0e7d6310df4c5
SebaGhost <sebaghost23@gmail.com>
2024-05-08 19:55:14 -0400

    enemigos 2.2
    Se agrego que el enemigo siga al personaje

---
+++ Assets/Script/Enemigo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    public GameObject Personaje;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direccion = Personaje.transform.position - transform.position;
+        if(direccion.x <=0.0f)
+    
+            transform.localScale = new Vector3 (1.0f , 1.0f , 1.0f);
+
+        
+        else
+            transform.localScale = new Vector3 (-1.0f , 1.0f , 1.0f);
+
+
+    }
+}
