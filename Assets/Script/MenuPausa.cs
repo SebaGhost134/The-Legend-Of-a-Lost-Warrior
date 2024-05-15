317d4910992ab9e6bcc295245beaa244fb5bec32
SuperBellaca <antonanami02@gmail.com>
2024-05-15 17:04:54 -0400

    MenuPausa
    Creación del menú, no tocar aún falta y también un script

---
+++ Assets/Script/MenuPausa.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Pausa(){
+        Time.timeScale = 0f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
