01ce46baa18a79efc5a5207ac5e1abf07aa9a1a7
SebaGhost <sebaghost23@gmail.com>
2024-06-14 20:01:17 -0400

    Nuevos Asets y cambios
    Se realizaron nuevos cambios en los pinchos y se agregaron nuevos assets

---
+++ Assets/Script/Pinchos.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pinchos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = false;
+        }
+    }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            // Verifica si el GameObject con el que colisionó tiene la etiqueta "Player"
+            if(other.gameObject.CompareTag("Player"))
+            {
+                // Llama al método PerderVida del GameManager
+                GameManager.Instance.PerderVida();
+            }
+        }
+}
